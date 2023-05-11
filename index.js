d945aa365d180fd3ac5a0d370884fdfc16745bd1
Lucas de Lara Nunes <lucas@gavelytics.com>
2023-05-11 15:04:56 -0300

    Initial deploy of the html page

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ if (localStorage.getItem('access_token') != null){
   console.log(access_token);
   document.getElementById('login').style.visibility = 'hidden';
   document.getElementById('logout').style.visibility = 'visible';
-  document.getElementById('search-form').style.visibility = 'visible';
+  document.getElementById('search').style.visibility = 'visible';
+  document.getElementById('query').style.visibility = 'visible';
 
   let search = document.getElementById('search')
   const queryInput = document.getElementById('query');
@@ -16,19 +17,13 @@ if (localStorage.getItem('access_token') != null){
   localStorage.setItem('access_token', access_token);
   document.getElementById('login').style.visibility = 'hidden';
   document.getElementById('logout').style.visibility = 'visible';
-  document.getElementById('search-form').style.visibility = 'visible';
+  document.getElementById('search').style.visibility = 'visible';
+  document.getElementById('query').style.visibility = 'visible';
 }
-
-
-
-//&& localStorage.getItem('access_token') == null
 
 async function logIn() {
     try {
       window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://lucasdelaranunes.github.io/fuzzy-memory/&prompt=consent&response_type=token&client_id=499617837412-k3gsjdg3hd9tg5u9ivdjdhnbj9ir30ki.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/drive.readonly&access_type=online";
-      //const response = await fetch("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://lucasdelaranunes.github.io/fuzzy-memory/&amp;prompt=consent&amp;response_type=token&amp;client_id=499617837412-k3gsjdg3hd9tg5u9ivdjdhnbj9ir30ki.apps.googleusercontent.com&amp;scope=https://www.googleapis.com/auth/drive.readonly&amp;access_type=online");
-      //const players = await response.json();
-      //console.log(players);
     } catch (e) {
       console.log(`Something went wrong while fetching the players.`);
       throw e;
