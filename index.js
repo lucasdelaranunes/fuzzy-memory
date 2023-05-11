eee7a8ec97d6467ba8e1774c91e24c22cbb09b1e
Lucas de Lara Nunes <lucas@gavelytics.com>
2023-05-11 14:45:29 -0300

    Initial deploy of the html page

--- index.js
+++ index.js
@@ -35,53 +35,53 @@ async function logIn() {
     }
   }
 
-  function logout() {
+function logOut() {
 
-    localStorage.removeItem('access_token');
+  localStorage.removeItem('access_token');
 
-    fetch("https://oauth2.googleapis.com/revoke?token=" + access_token,
-      {
-          method: 'POST',
-          headers: {
-              "Content-type": "application/x-www-form-urlencoded"
-          }
-      })
-      .then((data) => {
-          location.href = "https://lucasdelaranunes.github.io/fuzzy-memory/"
-      })
-  }
+  fetch("https://oauth2.googleapis.com/revoke?token=" + access_token,
+    {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded"
+        }
+    })
+    .then((data) => {
+        location.href = "https://lucasdelaranunes.github.io/fuzzy-memory/"
+    })
+}
     
-  function listFiles(){
-    const queryInput = document.getElementById('query');
-    searchFiles(`fullText contains '${queryInput.value}'`, 10)
-  }
+function listFiles(){
+  const queryInput = document.getElementById('query');
+  searchFiles(`fullText contains '${queryInput.value}'`, 10)
+}
 
-  function searchFiles(q="", pageSize){
-    document.getElementById('results-table').style.visibility = 'visible';
-    let result = document.getElementById('result')
-    result.innerHTML = ''
+function searchFiles(q="", pageSize){
+  document.getElementById('results-table').style.visibility = 'visible';
+  let result = document.getElementById('result')
+  result.innerHTML = ''
 
-    console.log(q)
+  console.log(q)
 
-    fetch(`https://www.googleapis.com/drive/v3/files?q=${q}&pageSize=${pageSize}&supportsAllDrives=true&fields=files(id,name,mimeType)`, {
-      method: 'GET',
-      headers:new Headers({Authorization:"Bearer " + info['access_token']})
-    })
-    .then((res) => res.json())
-    .then((info) => {
-      console.log(info)
-      info.files.forEach(file => {
-        let id = file.id
-        result.innerHTML += `
+  fetch(`https://www.googleapis.com/drive/v3/files?q=${q}&pageSize=${pageSize}&supportsAllDrives=true&fields=files(id,name,mimeType)`, {
+    method: 'GET',
+    headers:new Headers({Authorization:"Bearer " + info['access_token']})
+  })
+  .then((res) => res.json())
+  .then((info) => {
+    console.log(info)
+    info.files.forEach(file => {
+      let id = file.id
+      result.innerHTML += `
 
-        <tr>
-          <td>
-            <a target="_blank" href="https://drive.google.com/file/d/${file.id}">${file.name}</a>
-          </td>
-          <td>${file.mimeType}</td>
-        `
-      });
+      <tr>
+        <td>
+          <a target="_blank" href="https://drive.google.com/file/d/${file.id}">${file.name}</a>
+        </td>
+        <td>${file.mimeType}</td>
+      `
+    });
 
-    })
-  }
+  })
+}
   //LINK = https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://lucasdelaranunes.github.io/fuzzy-memory/&prompt=consent&response_type=token&client_id=499617837412-k3gsjdg3hd9tg5u9ivdjdhnbj9ir30ki.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/drive.readonly&access_type=online
