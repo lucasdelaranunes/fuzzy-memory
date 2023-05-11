6ab9ff3b29fcc3cc7aa4c2f022f43d36e919449d
Lucas de Lara Nunes <lucas@gavelytics.com>
2023-05-11 12:02:40 -0300

    Initial deploy of the html page

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+if (window.location.hash != ''){
+  access_token = window.location.hash.substring(14);
+  console.log(access_token);
+}
+
 async function logIn() {
     try {
-      window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://lucasdelaranunes.github.io/fuzzy-memory/&amp;prompt=consent&amp;response_type=token&amp;client_id=499617837412-k3gsjdg3hd9tg5u9ivdjdhnbj9ir30ki.apps.googleusercontent.com&amp;scope=https://www.googleapis.com/auth/drive.readonly&amp;access_type=online";
+      window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://lucasdelaranunes.github.io/fuzzy-memory/&prompt=consent&response_type=token&client_id=499617837412-k3gsjdg3hd9tg5u9ivdjdhnbj9ir30ki.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/drive.readonly&access_type=online";
       //const response = await fetch("https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=https://lucasdelaranunes.github.io/fuzzy-memory/&amp;prompt=consent&amp;response_type=token&amp;client_id=499617837412-k3gsjdg3hd9tg5u9ivdjdhnbj9ir30ki.apps.googleusercontent.com&amp;scope=https://www.googleapis.com/auth/drive.readonly&amp;access_type=online");
       //const players = await response.json();
       //console.log(players);
